e9c54a964f06b4ee980f60b1213775c0cd21afe8
Josh Perry <joshperry@outlook.com>
2022-12-02 21:50:53 +0000

    Day 2: part 2

--- day_02/__init__.py
+++ day_02/__init__.py
@@ -39,7 +39,19 @@ win_loss_draw_points_map = {
 }
 
 
-def parse_strategy_guide():
+move_intention_outcome_map = {
+    'RX': 'S',
+    'RY': 'R',
+    'RZ': 'P',
+    'PX': 'R',
+    'PY': 'P',
+    'PZ': 'S',
+    'SX': 'P',
+    'SY': 'S',
+    'SZ': 'R'
+}
+
+def parse_strategy_guide_part_1():
     input_file = open("day_02/input", "r")
     rounds = []
 
@@ -50,30 +62,52 @@ def parse_strategy_guide():
     return rounds
 
 
+def parse_strategy_guide_part_2():
+    input_file = open("day_02/input", "r")
+    rounds = []
+
+    for line in input_file.readlines():
+        split_line = line.rstrip().split(" ")
+
+        first_move = split_line[0]
+        second_move = move_intention_outcome_map[opponent_map[split_line[0]] + split_line[1]]
+
+        rounds.append([opponent_map[first_move], second_move])
+
+    return rounds
+
+
+def calculate_points(rounds):
+    total_points = 0
+
+    for r in rounds:
+        total_points += shape_points[r[1]]
+        total_points += win_loss_draw_points[win_loss_draw_points_map[r[0] + r[1]]]
+
+    return total_points
+
+
 def part_1():
     """
     What would your total score be if everything goes exactly according to your strategy guide?
     :return:
     """
     print("Part 1")
-    rounds = parse_strategy_guide()
-    total_points = 0
-
-    for r in rounds:
-        print(r[0], r[1])
-        total_points += shape_points[r[1]]
-        total_points += win_loss_draw_points[win_loss_draw_points_map[r[0] + r[1]]]
-
+    rounds = parse_strategy_guide_part_1()
+    total_points = calculate_points(rounds)
     print(total_points)
 
 
 def part_2():
     """
-
+    Following the Elf's instructions for the second column, what would your total score be if everything goes exactly
+    according to your strategy guide?
     :return:
     """
     print("Part 2")
-    pass
+    rounds = parse_strategy_guide_part_2()
+    total_points = calculate_points(rounds)
+    print(total_points)
 
 
 def go():
