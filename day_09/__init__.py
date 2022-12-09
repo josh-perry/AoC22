11fc779b29820f3309f9ca08773360a8fd133319
Josh Perry <joshperry@outlook.com>
2022-12-09 21:32:28 +0000

    Day 9: part 2

--- day_09/__init__.py
+++ day_09/__init__.py
@@ -41,17 +41,11 @@ def is_touching(p1, p2):
     return abs(p1.x - p2.x) <= 1 and abs(p1.y - p2.y) <= 1
 
 
-def part_1():
-    """
-    Simulate your complete hypothetical series of motions. How many positions does the tail of the rope visit at least
-    once?
-    :return:
-    """
-    print("Part 1")
-    instructions = parse_input()
-    head_position = Position(0, 0)
-    last_head_position = Position(0, 0)
-    tail_position = Position(0, 0)
+def count_tail_visits_for_rope_length(instructions, rope_length):
+    rope_positions = []
+
+    for rope_position_index in range(0, rope_length):
+        rope_positions.append(Position(0, 0))
 
     direction_offsets = {
         "U": (0, -1),
@@ -60,34 +54,62 @@ def part_1():
         "R": (1, 0)
     }
 
+    tail = rope_positions[-1]
+    head = rope_positions[0]
+
     tail_visits = set()
-    tail_visits.add((tail_position.x, tail_position.y))
+    tail_visits.add((tail.x, tail.y))
 
     for instruction in instructions:
         offset = direction_offsets[instruction.direction]
 
-        for step in range(0, instruction.distance):
-            last_head_position.x = head_position.x
-            last_head_position.y = head_position.y
-            head_position += offset
+        for d in range(0, instruction.distance):
+            head += offset
 
-            if is_touching(head_position, tail_position):
-                continue
+            for i in range(1, rope_length):
+                head_knot = rope_positions[i - 1]
+                knot = rope_positions[i]
 
-            tail_position.x = last_head_position.x
-            tail_position.y = last_head_position.y
-            tail_visits.add((tail_position.x, tail_position.y))
+                if is_touching(knot, head_knot):
+                    continue
 
-    print(len(tail_visits))
+                if head_knot.x - knot.x > 0:
+                    knot.x += 1
+                elif head_knot.x - knot.x < 0:
+                    knot.x -= 1
+
+                if head_knot.y - knot.y > 0:
+                    knot.y += 1
+                elif head_knot.y - knot.y < 0:
+                    knot.y -= 1
+
+                tail_visits.add((tail.x, tail.y))
+
+    return len(tail_visits)
+
+
+def part_1():
+    """
+    Simulate your complete hypothetical series of motions. How many positions does the tail of the rope visit at least
+    once?
+    :return:
+    """
+    print("Part 1")
+    instructions = parse_input()
+
+    print(count_tail_visits_for_rope_length(instructions, 2))
 
 
 def part_2():
     """
-
+    Simulate your complete series of motions on a larger rope with ten knots. How many positions does the tail of the
+    rope visit at least once?
     :return:
     """
     print("Part 2")
-    pass
+    instructions = parse_input()
+
+    print(count_tail_visits_for_rope_length(instructions, 10))
 
 
 def go():
